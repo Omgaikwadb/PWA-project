53a6ab8496927982da5621e07650cbc9bb8e27fe
Omgaikwadb <111499958+Omgaikwadb@users.noreply.github.com>
2024-04-02 00:15:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function (registration) {
+      console.log(
+        "Service worker registration successful:",
+        registration.scope
+      );
+    })
+    .catch(function (error) {
+      console.log("Service worker registration failed:", error);
+    });
+}
